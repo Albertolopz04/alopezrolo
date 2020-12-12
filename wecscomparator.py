d2f359e34fa86bb28a8017d211a8e6c7a27112ba
Alberto López Rolo <69103791+alopezrolo@users.noreply.github.com>
2020-12-12 17:20:32 +0100

    Update wecscomparator.py

--- wecscomparator.py
+++ wecscomparator.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
 # - Setting web configuration
 st.set_page_config(
