8433fe5f53a34da82f5a710984f1544f0a520408
Alberto López Rolo <69103791+alopezrolo@users.noreply.github.com>
2020-12-14 12:26:32 +0100

    Update wecscomparator.py

--- wecscomparator.py
+++ wecscomparator.py
@@ -12,75 +12,74 @@ st.set_page_config(
 
 # - Heading
 st.title('🔎 WECS Easy Choice v.2')
-st.write("""
-In this webapp you can *find* the **wind turbine** that best suits your needs.
-"""	)
-
-'This project is a webapp able to provide a list of candidate Wind Energy Conversion System (WECS) to the user after he has selected a couple parameters. The user is able to observe and compare freely any turbine of the around 300 WECS database whenever he wants.' 
-'Once the WECS are filtered the following values will be displayed'
-'* Manufacturer name'
-'* Nominal Power'
-'* Type'
-'* Blades Diameter'
-'* Onshore/Offshore'
-'* Website of the manufacturer'
-'* Power and performance (cp) curve'
-' '
-'---'
+st.write("""In this webapp you can *find* the **wind turbine** that best suits your needs."""	)
 # - Loading the data
 st.cache()
 wecs = pd.read_csv('300_wecsdata.csv', delimiter=';',  error_bad_lines=False, encoding='latin-1')
 
 # - Selecting the parameters
-st.write("""	### Select the rated power	""")
-rated_power_selected = st.slider('Rated Power (kW)', 0, 10000,2500,500 )
-'You selected a ', rated_power_selected/1000, 'MW WECS'
+st.sidebar.write("""	### Select the rated power	""")
+rated_power_selected = st.sidebar.slider('Rated Power (kW)', 0, 10000,2500,500 )
+#'You selected a ', rated_power_selected/1000, 'MW WECS'
 
 # 1.Definir intervalo del rated power
 interval = 501
 rated_power_min = rated_power_selected - interval
 rated_power_max = rated_power_selected + interval
-'The app will show you WECS between ', rated_power_min + 1, ' and ', rated_power_max - 1, 'kW'
+st.sidebar.write('The app will show you WECS between ', rated_power_min + 1, ' and ', rated_power_max - 1, 'kW')
 
 # 2.Seleccionar el tipo
-st.write("""	### Select the type of of wind turbine	""")
-type_selected=st.multiselect('Type', ['1','2','3','4'],['1','2','3','4'])
+st.sidebar.write("""	### Select the type of wind turbine	""")
+#st.sidebar.write(
+#""" If you dont know what this mean check this [article](https://solarfeeds.com/types-of-wind-turbine-generators-and-their-functions/)"""
+#)
+type_selected=st.sidebar.multiselect('Type', ['1','2','3','4'],['1','2','3','4'])
 
 # 3.Seleccionar Onshore/Offshore
-st.write("""	### Select whether the WECS is designed for Onshore/Offshore	""")
-#offshore_selected = st.selectbox('Onshore/Offshore',['Onshore', 'Offshore'])
-shore = st.radio("Select if the WECS you are looking for is going to be at see or at land.",('Onshore','Offshore'))
+st.sidebar.write("""	### Select whether the WECS is designed for Onshore/Offshore""")
+shore = st.sidebar.radio("Select if the WECS you are looking for is going to be at see or at land.",('Onshore','Offshore'))
 of = shore
 if of == 'Offshore':
 	of = 1
 elif of == 'Onshore':
 	of = 0
-''
+
 # 4.Seleccionar el fabricante
-manufacturer_selected = 'All'
-if st.checkbox('Select manufacturer'):
-	st.write("""	### Filter by manufacturer	""")
-	st.write('This is optional, if you don\'t want to filter by manufacturer deselect the checkbox')
+st.sidebar.write("""	### Filter by manufacturer	""")
+manufacturer_selected = ' '
+if st.sidebar.checkbox('Select manufacturer'):
+	
 	# Convert the csv file into a list to display it on the select box
 	manufacturer = pd.read_csv('manufacturerList.csv', delimiter=';', error_bad_lines=False, encoding='utf-8')
 	brand = manufacturer['manufacturerName'].tolist()
 	brand.append(' ')
-	manufacturer_selected = st.selectbox('Choose the manufacturer', brand,58)
+	manufacturer_selected = st.sidebar.selectbox('Choose the manufacturer', brand,58)
 
 	if manufacturer_selected == ' ':
-		manufacturer_selected = 0
+		manufacturer_selected = ' '
 	elif manufacturer_selected == 'Acciona':
 		manufacturer_selected = 1
 	else:
 		manufacturer_selected = manufacturer[manufacturer['manufacturerName']==manufacturer_selected].iloc[0,0]
 
+st.sidebar.write('---')
+if st.sidebar.checkbox('More information'):
+	st.sidebar.write('This project is a webapp able to provide a list of candidate Wind Energy Conversion System (WECS) to the user after he has selected a couple parameters. The user is able to observe and compare freely any turbine of the around 300 WECS database whenever he wants.' 
+	'Once the WECS are filtered the following values will be displayed'
+	'* Manufacturer name'
+	'* Nominal Power'
+	'* Type'
+	'* Blades Diameter'
+	'* Onshore/Offshore'
+	'* Website of the manufacturer'
+	'* Power and performance (cp) curve')
+	' '
 
 '## Results 📊'
-
 # - Loading the data
 df = pd.read_csv('99_wecs_data.csv', delimiter=';', error_bad_lines=False, encoding='latin-1')
 # Filterig data
-if manufacturer_selected != 'All':
+if manufacturer_selected != ' ':
 	wecs_selected = wecs[(wecs.brandID==manufacturer_selected) & (wecs.datavp=='v') & (wecs['power'].between(rated_power_min,rated_power_max, inclusive=False)) & (wecs["type"].isin(type_selected)) & (wecs["offshore?"]==of)]
 else:
 	wecs_selected = wecs[(wecs.datavp=='v') & (wecs['power'].between(rated_power_min,rated_power_max, inclusive=False)) & (wecs["type"].isin(type_selected)) & (wecs["offshore?"]==of)]
@@ -103,7 +102,7 @@ else:
 '### Plotting the results'
 wecsSeries = pd.DataFrame()
 legend = []
-st.warning('You should only check this once you have narrowed down the search to a handful number of WECS.\n Otherwise, the app will be slowed down.')
+st.info('Check once you have narrowed down the search to a handful of WECS.')
 if st.checkbox('Show the power curve of the results'):
 	'#### Graph of all the WECS that meet the user criteria'
 	fig, ax = plt.subplots()
